1d60b7d1bb42c29365a5906b16c62af624f4a50f
kazvee <kazveedev@gmail.com>
2024-04-22 21:52:04 -0600

    feat: add player movement & animations

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { k } from './kaboomContext';
+
+k.loadSprite('spritesheet', './spritesheet.png', {
+  sliceX: 39,
+  sliceY: 31,
+  anims: {
+    'idle-down': 956,
+    'walk-down': { from: 956, to: 959, loop: true, speed: 8 },
+    'idle-side': 995,
+    'walk-side': { from: 995, to: 998, loop: true, speed: 8 },
+    'idle-up': 1034,
+    'walk-up': { from: 1034, to: 1037, loop: true, speed: 8 },
+  },
+});
