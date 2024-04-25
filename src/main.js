68c012a1b71e74a82dbaff7108d8bbd11ef64865
kazvee <kazveedev@gmail.com>
2024-04-25 16:01:39 -0600

    feat: implement random player character generation

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
+import { characters } from './characters';
 import { dialogueData, scaleFactor } from './constants';
 import { k } from './kaboomContext';
 import { displayDialogue, setCamScale } from './utils';
 
+const randomCharacter =
+  characters[Math.floor(Math.random() * characters.length)];
+
 k.loadSprite('spritesheet', './spritesheet.png', {
   sliceX: 39,
   sliceY: 31,
-  anims: {
-    'idle-down': 956,
-    'walk-down': { from: 956, to: 959, loop: true, speed: 8 },
-    'idle-side': 995,
-    'walk-side': { from: 995, to: 998, loop: true, speed: 8 },
-    'idle-up': 1034,
-    'walk-up': { from: 1034, to: 1037, loop: true, speed: 8 },
-  },
+  anims: randomCharacter.animations,
 });
 
 k.loadSprite('map', './map.png');
