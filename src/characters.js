99eb2ba66158d7eb41dae3c7c281137c6fa8c5b6
kazvee <kazveedev@gmail.com>
2024-04-25 16:01:23 -0600

    feat: add predefined player characters list

---
+++ src/characters.js
@@ -0,0 +1,90 @@
+export const characters = [
+  {
+    name: 'Avery',
+    animations: {
+      'idle-down': 936,
+      'walk-down': { from: 936, to: 939, loop: true, speed: 8 },
+      'idle-side': 975,
+      'walk-side': { from: 975, to: 978, loop: true, speed: 8 },
+      'idle-up': 1014,
+      'walk-up': { from: 1014, to: 1017, loop: true, speed: 8 },
+    },
+  },
+  {
+    name: 'Blair',
+    animations: {
+      'idle-down': 940,
+      'walk-down': { from: 940, to: 943, loop: true, speed: 8 },
+      'idle-side': 979,
+      'walk-side': { from: 979, to: 982, loop: true, speed: 8 },
+      'idle-up': 1018,
+      'walk-up': { from: 1018, to: 1021, loop: true, speed: 8 },
+    },
+  },
+  {
+    name: 'Casey',
+    animations: {
+      'idle-down': 944,
+      'walk-down': { from: 944, to: 947, loop: true, speed: 8 },
+      'idle-side': 983,
+      'walk-side': { from: 983, to: 986, loop: true, speed: 8 },
+      'idle-up': 1022,
+      'walk-up': { from: 1022, to: 1025, loop: true, speed: 8 },
+    },
+  },
+  {
+    name: 'Dakota',
+    animations: {
+      'idle-down': 948,
+      'walk-down': { from: 948, to: 951, loop: true, speed: 8 },
+      'idle-side': 987,
+      'walk-side': { from: 987, to: 990, loop: true, speed: 8 },
+      'idle-up': 1026,
+      'walk-up': { from: 1026, to: 1029, loop: true, speed: 8 },
+    },
+  },
+  {
+    name: 'Ellis',
+    animations: {
+      'idle-down': 952,
+      'walk-down': { from: 952, to: 955, loop: true, speed: 8 },
+      'idle-side': 991,
+      'walk-side': { from: 991, to: 994, loop: true, speed: 8 },
+      'idle-up': 1030,
+      'walk-up': { from: 1030, to: 1033, loop: true, speed: 8 },
+    },
+  },
+  {
+    name: 'Frankie',
+    animations: {
+      'idle-down': 956,
+      'walk-down': { from: 956, to: 959, loop: true, speed: 8 },
+      'idle-side': 995,
+      'walk-side': { from: 995, to: 998, loop: true, speed: 8 },
+      'idle-up': 1034,
+      'walk-up': { from: 1034, to: 1037, loop: true, speed: 8 },
+    },
+  },
+  {
+    name: 'Georgie',
+    animations: {
+      'idle-down': 960,
+      'walk-down': { from: 960, to: 963, loop: true, speed: 8 },
+      'idle-side': 999,
+      'walk-side': { from: 999, to: 1002, loop: true, speed: 8 },
+      'idle-up': 1038,
+      'walk-up': { from: 1038, to: 1041, loop: true, speed: 8 },
+    },
+  },
+  {
+    name: 'Hayden',
+    animations: {
+      'idle-down': 964,
+      'walk-down': { from: 964, to: 967, loop: true, speed: 8 },
+      'idle-side': 1003,
+      'walk-side': { from: 1003, to: 1006, loop: true, speed: 8 },
+      'idle-up': 1042,
+      'walk-up': { from: 1042, to: 1045, loop: true, speed: 8 },
+    },
+  },
+];
