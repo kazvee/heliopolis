ee1633d55b44330c790a2e12b11faf2312624ad7
kazvee <kazveedev@gmail.com>
2025-01-19 18:43:08 -0700

    feat: add more dialogue & alphabetize sections

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,75 @@
-export const scaleFactor = 4;
+export const scaleFactor = 1; // TODO: change back to 4 when finished testing
 
 export const dialogueData = {
-  'treasure_chest': "I wonder what's in here...",
-  'welcome': "Welcome to Heliopolis! This is a fan website where you can learn more about <a href='https://heliohost.org/' target='_blank'>HelioHost</a> and their services.<p>All details have been sourced from the <a href='https://wiki.helionet.org/' target='_blank'>HelioHost Wiki</a>.<p>Please feel free to interact with your surroundings: Read signs, hug trees, talk to the wildlife, and walk around some more.<p>Running is strictly forbidden.",
-  'one': "Like most places, there are some <a href='https://wiki.helionet.org/Terms' target='_blank'>rules</a> everone needs to follow. Here's a big 1!<br><br>What does the big 1 have to do with the rules? Well, everyone is only allowed <strong>one</strong> account on HelioHost. 1. One. <strong>ONE</strong>. People who make more than one account will be <a href='https://wiki.helionet.org/Suspension_Policy#Duplicate_Accounts' target='_blank'>suspended</a>. Since we can have <a href='https://wiki.helionet.org/Addon_Domains' target='_blank'>up to 10 domains on an account</a>, and even <a href='https://wiki.helionet.org/Donations_for_Increased_Storage' target='_blank'>upgrade our storage</a> if needed, there's really no need for multiple accounts anyway. <p>This rule helps HelioHost provide free accounts to as many people as possible.",
+  // #
+  '1': "Like most places, HelioHost has some <a href='https://wiki.helionet.org/Terms' target='_blank'>rules</a> everyone needs to follow. Here's a big 1!<p>What does the big 1 have to do with the rules? Well, everyone is only allowed <strong>one</strong> account on HelioHost. 1. One. <strong>ONE</strong>. People who make more than one account will be <a href='https://wiki.helionet.org/Suspension_Policy#Duplicate_Accounts' target='_blank'>suspended</a>.<p>This rule helps HelioHost provide free accounts to as many people as possible.",
+  '20': "<a href='https://heliohost.org/' target='_blank'>HelioHost</a> has been offering free web hosting services to the community for over 20 years!<p>You can <a href='https://heliohost.org/donate/' target='_blank'>make a donation</a> to help support their efforts, or check out some <a href='https://wiki.helionet.org/How_You_Can_Help' target='_blank'>other ways you can help</a>.",
+  '404': "Whoa, that's a big error! Did you know that HelioHost has a big help section for <a href='https://wiki.helionet.org/Common_Errors' target='_blank'>Common Errors</a>? You can get all kinds of help there!",
+
+  // A
+
+  // B
+  'binary-tree': "01101000 01100101 01101100 01101100 01101111 00001010<p>Hmm...<p>I wonder what that means...",
+
+  // C
   'cpu': "HelioHost accounts have a limit of <a href='https://wiki.helionet.org/Suspension_Policy#Account_Load_Limits' target='_blank'>no more than 10,000 CPU usage</a> per day.<p>If that's not enough, they also have <a href='https://wiki.helionet.org/Morty' target='_blank'>scaling hosting</a>! For people who need even more CPUs, they offer a range of <a href='https://heliohost.org/vps/' target='_blank'>VPS plans</a>, with a 10% discount when paying for 6 months upfront.",
+
+  // D
+
+  // E
+
+  // F
+  'fireplace': "< fireplace noises >",
+  'firewall': "Hmm...<p>Oh, wait...<p>It's a <em>firewall</em>!",
+  'frog': "I wasn't always a frog, you know...<p><em>< sad frog noises ></em><p>If only I had followed the <a href='https://wiki.helionet.org/Terms' target='_blank'>rules</a>!",
+
+  // G
+  'green-grass': "When something in a game looks a little out of place, sometimes it's hiding a secret!<p>But not here. This is just a patch of really green grass.<p>I'm going to go read the <a href='https://helionet.org/index/forum/1-news/' target='_blank'>HelioNet forum News section</a> now.",
+
+  // H
+
+  // I
+  'internet': "A... series of tubes? This must be the internet!",
+
+  // J
+
+  // K
+
+  // L
+
+  // M
   'memory': "HelioHost accounts have a memory limit of <a href='https://wiki.helionet.org/Suspension_Policy#Account_Load_Limits' target='_blank'>no more than 100 GB</a> per day.<p>If that's not enough, they also have <a href='https://wiki.helionet.org/Morty' target='_blank'>scaling hosting</a>! For people who need even more memory, they offer a range of <a href='https://heliohost.org/vps/' target='_blank'>VPS plans</a>, with a 10% discount when paying for 6 months upfront.",
-  'frog': "I wasn't always a frog, you know...<p><em>*sad frog noises*</em><p>If only I had followed the <a href='https://wiki.helionet.org/Terms' target='_blank'>rules</a>!",
-  '20': "<a href='https://heliohost.org/' target='_blank'>HelioHost</a> has been offering free web hosting services to the community for over 20 years!<br><br>You can <a href='https://heliohost.org/donate/' target='_blank'>make a donation</a> to help support their efforts, or check out some <a href='https://wiki.helionet.org/How_You_Can_Help' target='_blank'>other ways you can help</a>.",
+
+  // N
+
+  // O
+
+  // P
+
+  // Q
+
+  // R
+
+  // S  
+
+  // T
+  'terms': "These are HelioHost's <a href='https://heliohost.org/terms/' target='_blank'>Terms of Service</a>! I better check this out to make sure I don't break any rules and get my <a href='https://wiki.helionet.org/Suspension_Policy' target='_blank'>account suspended</a>.",
+  'tower-left': "I wonder what's in there...",
+  'tower-right': "< weird noises ><p>Umm... I better leave this alone.",
+  'treasure-chest': "I wonder what's in here...",
+  'treasure-rug': "A treasure chest? And it's open?! There's a note inside! Maybe it's a map? A list of magic spells?! I'm going to read it! It says...<p>Hmm... It says... 'Please do not walk on the rug.'<p>Grr! This note could have been an email! Speaking of email, HelioHost has a great <a href='https://wiki.helionet.org/Email_Delivery_Issues' target='_blank'>troubleshooting help page</a> for any mail delivery issues you might have.",
+
+  // U
+
+  // V
+
+  // W
+  'welcome': "Welcome to Heliopolis! This is a fan website where you can learn more about <a href='https://heliohost.org/' target='_blank'>HelioHost</a> and their services.<p>All details have been sourced from the <a href='https://wiki.helionet.org/' target='_blank'>HelioHost Wiki</a>.<p>Please feel free to interact with your surroundings: Read signs, hug trees, talk to the wildlife, and walk around some more.<p>Running is strictly forbidden.",
+  'wiki': "HelioHost has a <a href='https://wiki.helionet.org/' target='_blank'>Wiki</a>, too?! Awesome!<p>I bet other folks have asked the same questions I have. I guess I'll find my answers in the <a href='https://wiki.helionet.org/FAQ' target='_blank'>FAQ</a> section!<p>First up: figuring out what 'FAQ' even means...<p>Then I'll build something cool with the help of their <a href='https://wiki.helionet.org/Tutorials' target='_blank'>tutorials</a>!",
+
+  // X
+
+  // Y
+
+  // Z
 };
