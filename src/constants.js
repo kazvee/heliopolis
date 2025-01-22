bd7d1c135d478e908477abe76ad2206e6607c6f4
kazvee <kazveedev@gmail.com>
2025-01-21 20:14:50 -0700

    feat: add dialogue

--- src/constants.js
+++ src/constants.js
@@ -8,8 +8,7 @@ export const dialogueData = {
   '404': "Whoa, that's a big error! Did you know that HelioHost has a big help section for <a href='https://wiki.helionet.org/Common_Errors' target='_blank'>Common Errors</a>? You can get all kinds of help there!",
 
   // A  
-  'account-load': "I check my <a href='https://heliohost.org/dashboard/load/' target='_blank'>account load</a> every so often, to make sure I don't risk <a href='https://wiki.helionet.org/Suspension_Policy#High_Server_Usage' target='_blank'>account suspension</a>.<p>Sofa, so good! ha ha ha!<p>",
-
+  
   // B
   'binary-tree': "01110100 01110010 01100101 01100101<p>Hmm...<p>I wonder what that means...",
   'bookcase': "HelioHost's <a href='https://wiki.helionet.org/Tutorials' target='_blank'>tutorial section</a> is like a big bookcase full of information! I can build all kinds of things with the help of their how-to guides!",
@@ -18,15 +17,17 @@ export const dialogueData = {
   'cpu': "HelioHost accounts have a limit of <a href='https://wiki.helionet.org/Suspension_Policy#Account_Load_Limits' target='_blank'>no more than 10,000 CPU usage</a> per day.<p>If that's not enough, they also have <a href='https://wiki.helionet.org/Morty' target='_blank'>scaling hosting</a>! For people who need even more CPUs, they offer a range of <a href='https://heliohost.org/vps/' target='_blank'>VPS plans</a>, with a 10% discount when paying for 6 months upfront.",
 
   // D
-  'database': "What a nice table! Is it... mahogany?<p>Tables are great! At HelioHost, we can create <a href='https://wiki.helionet.org/PostgreSQL' target='_blank'>PostgreSQL</a>, <a href='https://wiki.helionet.org/MySQL' target='_blank'>MySQL/MariaDB</a>, and <a href='https://wiki.helionet.org/SQLite' target='_blank'>SQLite</a> databases and use phpMyAdmin to manage them!",
+
 
   // E
 
   // F
   'fireplace': "< fireplace noises >",
   'firewall': "Hmm...<p>Oh, wait...<p>It's a <em>firewall</em>!<p>It's a lot more fiery than I expected...",
+  'forest-green': "Oh, hello! Uhh, I mean...<p>< tree noises >",
   'forum-news': "I've seen a few emails from HelioHost but I forgot what they said. I better check the <a href='https://heliohost.org/index/forum/1-news/' target='_blank'>HelioNet forum News section</a> to catch up on any important service announcements!",
   'frog': "I wasn't always a frog, you know...<p><em>< sad frog noises ></em><p>If only I had followed the <a href='https://wiki.helionet.org/Terms' target='_blank'>rules</a>!",
+  'frog-side': "Who, me? Yeah, I've always been a frog!<p>At least... I <em>think</em> I have.<p>< ribbit >",
 
   // G
   'green-grass': "When something in a game looks a little out of place, sometimes it's hiding a secret!<p>But not here. This is just a patch of really green grass.<p>I'm going to go read the <a href='https://helionet.org/index/forum/1-news/' target='_blank'>HelioNet forum News section</a> now.",
@@ -34,7 +35,8 @@ export const dialogueData = {
   // H
 
   // I
-  'internet': "A series of tubes? Whoa, this must be the internet!",
+  
+  'internet': "This looks like... a series of tubes? Whoa, it must be the internet!",
 
   // J
   'johnny': "This is the free <a href='https://wiki.helionet.org/Johnny' target='_blank'>Johnny server</a>! He's <a href https://wiki.helionet.org/Signing_Up#Helpful_Tips_for_Getting_a_Free_Account_on_Johnny target='_blank'>incredibly popular</a> since he hosts all of HelioHost's free accounts!",
@@ -58,18 +60,23 @@ export const dialogueData = {
 
   // P
   'plesk': "I'm going to <a href='https://wiki.helionet.org/Account_Backups#Making_Your_Own_Manual_Account_Backup' target='_blank'>backup my account</a> right now and download a copy to my local computer!<p>I worked so hard on my site, I should definitely save a copy of it somewhere safe, just in case!",
+  'potion-storage': "Whoever left all this stuff on the floor clearly needs a better storage solution.<p>If I ever need more than the <a href='https://wiki.helionet.org/Storage' target='_blank'>1000 MB of disk space</a> that comes with my standard HelioHost account, I can make a <a href='https://wiki.helionet.org/Donations_for_Increased_Storage' target='_blank'>one-time donation</a> to add more storage, increasing my filespace up to a total of 6000 MB. That would hold so many memes...",
 
   // Q
 
   // R
-  'red-button': "A big red button in the middle of nowhere? It's <emd>definitely</emd> a good idea to push this!<p>< click ><p>Ooh, this must be the <a href='https://wiki.helionet.org/Softaculous' target='_blank'>Softaculous</a> 'one-click' installer! I can use this to easily install all kinds of popular software packages!<p>< clicks again >",
+  'red-button': "A big red button in the middle of nowhere? It's <em>definitely</em> a good idea to push this!<p>< click ><p>Ooh, this must be the <a href='https://wiki.helionet.org/Softaculous' target='_blank'>Softaculous</a> 'one-click' installer! I can use this to easily install all kinds of popular software packages!<p>< clicks again >",
 
   // S
+  'server-reboot': "",
   'server-status': "A real-time <a href='https://heliohost.org/serverstatus/' target='_blank'>server status</a> page?! Neat! This is the first place I'll check if I see notice anything unusual happening with my site!",
-  'storage': "Whoever left all this stuff on the floor clearly needs a better storage solution.<p>If I ever need more than the <a href='https://wiki.helionet.org/Storage' target='_blank'>1000 MB of disk space</a> that comes with my standard HelioHost account, I can make a <a href='https://wiki.helionet.org/Donations_for_Increased_Storage' target='_blank'>one-time donation</a> to add more storage, increasing my filespace up to a total of 6000 MB. That would hold so many memes...",
-
+  'side-table-red': "To make sure that old, unused accounts don't fill up the servers, HelioHost has an <a href='https://wiki.helionet.org/Suspension_Policy#Inactivity_Policy' target='_blank'>inactivity policy</a> on the <a href='https://wiki.helionet.org/Tommy' target='_blank'>Tommy</a> and <a href='https://wiki.helionet.org/Johnny' target='_blank'>Johnny</a> servers. I guess making sure that accounts stay active means they can keep finding room for new users to join!<p>When I travel to the moon, I'll be gone longer than a month for sure. I'll make sure to move my account <a href='https://wiki.helionet.org/Morty' target='_blank'>Morty</a> before then. He's their flagship hosting server, and as long as I have a positive balance in the account, I won't have to worry about inactivity suspensions while I'm up in space!",
+  'sofa-account-load': "I check my <a href='https://heliohost.org/dashboard/load/' target='_blank'>account load</a> every so often, to make sure I don't risk <a href='https://wiki.helionet.org/Suspension_Policy#High_Server_Usage' target='_blank'>account suspension</a>.<p>Sofa, so good! ha ha ha!<p>",
+  
   // T
-  'terms': "These are HelioHost's <a href='https://heliohost.org/terms/' target='_blank'>Terms of Service</a>! I better check this out to make sure I don't break any rules and get my <a href='https://wiki.helionet.org/Suspension_Policy' target='_blank'>account suspended</a>.",
+  
+  'table-terms': "These are HelioHost's <a href='https://heliohost.org/terms/' target='_blank'>Terms of Service</a>! I better check this out to make sure I don't break any rules and get my <a href='https://wiki.helionet.org/Suspension_Policy' target='_blank'>account suspended</a>.",
+  'table-wiki': "HelioHost has a <a href='https://wiki.helionet.org/' target='_blank'>Wiki</a>, too?! Awesome!<p>I bet other folks have asked the same questions I have. I guess I'll find my answers in the <a href='https://wiki.helionet.org/FAQ' target='_blank'>FAQ</a> section!<p>First up: figuring out what 'FAQ' even means...<p>Then I'll build something cool with the help of their <a href='https://wiki.helionet.org/Tutorials' target='_blank'>tutorials</a>!",
   'tommy': "This is the <a href='https://wiki.helionet.org/Tommy' target='_blank'>Tommy</a> server</a>! He's fast, reliable, and available only to donors. for as little as $1 USD, you can <a href='https://heliohost.org/tommy/' target='_blank'>get an account on Tommy</a> any time of day!",
   'tower-left': "I wonder what's in there...",
   'tower-right': "< weird noises ><p>Umm... I better leave this alone.",
@@ -82,8 +89,7 @@ export const dialogueData = {
   'vps': "These are the <a href='https://wiki.helionet.org/' target='_blank'>VPS servers</a>! Wanna know a secret?<p>Well... If you pay for 6 months upfront, you get a <strong>10% discount</strong> on any of the VPS plans!<p>I guess it's not really a secret though, it says it right on the page after you choose a specific VPS plan. But still, it's a great deal!",
 
   // W
-  'welcome': "Welcome to Heliopolis! This is a fan website where you can learn more about <a href='https://heliohost.org/' target='_blank'>HelioHost</a> and their services.<p>All details have been sourced from the <a href='https://wiki.helionet.org/' target='_blank'>HelioHost Wiki</a>.<p>Please feel free to interact with your surroundings: Read signs, hug trees, talk to the wildlife, and walk around some more.<p>Running is strictly forbidden.",
-  'wiki': "HelioHost has a <a href='https://wiki.helionet.org/' target='_blank'>Wiki</a>, too?! Awesome!<p>I bet other folks have asked the same questions I have. I guess I'll find my answers in the <a href='https://wiki.helionet.org/FAQ' target='_blank'>FAQ</a> section!<p>First up: figuring out what 'FAQ' even means...<p>Then I'll build something cool with the help of their <a href='https://wiki.helionet.org/Tutorials' target='_blank'>tutorials</a>!",
+  'welcome': "Welcome to Heliopolis! This is a fan website where you can learn more about <a href='https://heliohost.org/' target='_blank'>HelioHost</a> and their services.<p>All details have been sourced from the <a href='https://wiki.helionet.org/' target='_blank'>HelioHost Wiki</a>.<p>Please feel free to interact with your surroundings: Read signs, hug trees, talk to the wildlife, and walk around some more.<p>Running is strictly forbidden.",  
 
   // X
 
